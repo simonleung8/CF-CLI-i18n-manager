825708cbc89aaf2628a740f30cd4bc893ec77f6e
Simon Leung <contact@simonleung.info>
2016-08-09 16:41:45 -0700

    Sort and overwrite langauge file

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"sort"
 
 	"github.com/simonleung8/flags"
 )
@@ -31,7 +32,6 @@ func init() {
 }
 
 func main() {
-
 	inputFile := fc.String("i")
 
 	if _, err := os.Stat(inputFile); os.IsNotExist(err) {
@@ -50,6 +50,20 @@ func main() {
 
 	translations := i18n_resources{}
 	err = json.Unmarshal(b, &translations)
+	if err != nil {
+		fmt.Fprintln(logger, "Error unmarshalling translations:", err)
+		os.Exit(1)
+	}
 
-	fmt.Printf("%#v\n", len(translations))
+	sort.Sort(i18n_resources(translations))
+
+	os.Remove(inputFile)
+
+	translationsJson, _ := json.MarshalIndent(translations, "", "  ")
+
+	err = ioutil.WriteFile(inputFile, translationsJson, 0755)
 }
+
+func (t i18n_resources) Len() int           { return len(t) }
+func (t i18n_resources) Swap(i, j int)      { t[i], t[j] = t[j], t[i] }
+func (t i18n_resources) Less(i, j int) bool { return t[i].ID < t[j].ID }
